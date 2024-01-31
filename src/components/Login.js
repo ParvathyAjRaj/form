b599edc3212f0a92623697b18b24d6e2e18edccc
ParvathyAjRaj <paruvathy99@gmail.com>
2024-01-31 17:47:40 +0530

    added validations for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,18 +8,33 @@ function Login(){
     const inputUser = useRef();
     const inputPwd = useRef();
     const [username,setUsername] = useState(null);
+    const [alertUsername,setAlertUsername] = useState(false);
     const [password,setPassword] = useState(null);
+    const [alertPassword,setAlertPassword] = useState(false);
 
     function handleUser(){
         setUsername(inputUser.current.value);
     }
+    
 
     function handlePwd(){
         setPassword(inputPwd.current.value);
     }
 
     function handleAdd(){
-        login_form.push(username);
+        if (username !== null && username.length >= 4){
+            setAlertUsername(false);
+            login_form.push(username);
+        }else{
+            setAlertUsername(true);
+        }
+        if(password !== null && password.length >=8 && password.length <=25){
+            setAlertPassword(false);
+            login_form.push(password);
+        }
+        else{
+            setAlertPassword(true);
+        }
     }
 
     return(
@@ -28,10 +43,15 @@ function Login(){
                 <label>Username</label>
                 <input type="text" ref={inputUser} onChange={handleUser} ></input>
             </div>
+            <div className="alert">
+                    {alertUsername === true ? <alert>Your username should be atleast 4 letters</alert> : null}
+            </div>
             <div className="pwd">
                 <label className="label">Password</label>
                 <input type="password" ref={inputPwd} onChange={handlePwd}></input>  
-            
+            </div>
+            <div className="alert">
+                    {alertPassword === true ? <alert>Your password should be atleast 8 letters length [maximum of 25 letters length]</alert> : null}
             </div>
             <div>
                 <button onClick={handleAdd}>Add</button>
